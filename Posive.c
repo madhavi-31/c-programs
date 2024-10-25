a51f04ad254feb1b4acad34c377cb0215f678179
madhavi-31 <madhaviloya31@gmail.com>
2024-10-25 11:58:52 +0530

    Create Posive.c

---
+++ Posive.c
@@ -0,0 +1,12 @@
+# include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the number:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("the number is positive ");
+    }
+    return 0;
+}
