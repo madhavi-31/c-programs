455d69dee19e81d77f5aef9dbd8f7c55cba75ca1
madhavi-31 <madhaviloya31@gmail.com>
2024-10-25 12:07:20 +0530

    Create Posive or negative.c

---
+++ Posive or negative.c
@@ -0,0 +1,16 @@
+# include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the number:");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("The number is positive ");
+    }
+    else
+    {
+        printf(" The given number is negative ");
+    }
+    return 0;
+}
