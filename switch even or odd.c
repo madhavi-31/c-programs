881ccc5b1866e05840bb6f5227c272fbc363d3cd
madhavi-31 <madhaviloya31@gmail.com>
2024-10-30 15:33:34 +0530

    Create switch even or odd.c

---
+++ switch even or odd.c
@@ -0,0 +1,13 @@
+**#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    switch(a%2)
+    {
+    case 0:printf("the given number is even");
+    break;
+    default:printf("the given number is odd");
+    break;
+    }
+}**
