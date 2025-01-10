445d3e081ac983e1b852f7d906da3684ce19f021
madhavi-31 <madhaviloya31@gmail.com>
2025-01-10 11:27:42 +0530

    Create Array avg.c

---
+++ Array avg.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main() 
+{
+    int a[10],sum=0,i, n;
+    float avg;
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        sum=sum+a[i];
+    }
+    avg=sum/n;
+    printf("%f", avg);
+    return 0;
+}
