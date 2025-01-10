1675c12a48a66ce6cf6ae57ae0f180fbf3810596
madhavi-31 <madhaviloya31@gmail.com>
2025-01-10 11:31:39 +0530

    Create Array copy.c

---
+++ Array copy.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main() 
+{
+    int a[10], b[10],i, n;
+    scanf("%d", &n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        b[i]=a[i];
+    }
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t", b[i]);
+    }
+    return 0;
+}
